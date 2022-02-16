bcb1a2f7e9817544fb81396765f77ae0ef3ce39e
wither7007 <jimsteffes@outlook.com>
2022-02-15 18:45:42 -0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react'
+import './index.css';
+import Tasks from './components/Tasks'
+import Header from './components/Header'
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      "id": 1,
+      "text": "Doctors Appointment",
+      "day": "Feb 5th at 2:30pm",
+      "reminder": true
+    },
+    {
+      "id": 2,
+      "text": "Meeting at School",
+      "day": "Feb 6th at 1:30pm",
+      "reminder": true
+    },
+    {
+      "id": 3,
+      "text": "My new Text",
+      "day": "Feb 6th at 1:30pm",
+      "reminder": true
+    }
+  ])
+  const deleteTask = (id) => {
+
+    console.log('delete', id)
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header title="react todo" />
+        <Tasks tasks={tasks} onDelete={deleteTask} />
       </header>
     </div>
   );
