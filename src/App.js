dea7ae4e7d3533ce5e552a2167965b137b8797d5
wither7007 <jimsteffes@outlook.com>
2022-02-17 16:32:36 -0600

    ok

--- src/App.js
+++ src/App.js
@@ -2,37 +2,47 @@ import { useState } from 'react'
 import './index.css';
 import Tasks from './components/Tasks'
 import Header from './components/Header'
+import Addtask from './Addtask';
 function App() {
   const [tasks, setTasks] = useState([
     {
       "id": 1,
       "text": "Doctors Appointment",
       "day": "Feb 5th at 2:30pm",
-      "reminder": true
+      "reminder": false
     },
     {
       "id": 2,
       "text": "Meeting at School",
       "day": "Feb 6th at 1:30pm",
-      "reminder": true
+      "reminder": false
     },
     {
       "id": 3,
       "text": "My new Text",
       "day": "Feb 6th at 1:30pm",
-      "reminder": true
+      "reminder": false
     }
   ])
   const deleteTask = (id) => {
-
     console.log('delete', id)
     setTasks(tasks.filter((task) => task.id !== id))
+  }
+  //Toggle reminder
+  const toggleReminder = (id) => {
+    console.log(id)
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    )
   }
   return (
     <div className="container">
       <header className="App-header">
-        <Header title="react todo" />
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Header title="Task Tracker" />
+        <Addtask />
+        {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "No tasks"}
       </header>
     </div>
   );
